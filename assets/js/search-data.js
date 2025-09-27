4021f91729d3bc58ef4661640315b3030d087e12
silviateliz <silvia.teliz@asc.upenn.edu>
2025-09-27 22:34:54 +0000

    Deploying to gh-pages from @ silviateliz/silviateliz.github.io@7368d3fad326d33b687723c2693fd45477995875 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -464,6 +464,13 @@ ninja.data = [{
           window.open("https://github.com/silviateliz", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/silvia-teliz", "_blank");
+        },
+      },{
         id: 'social-orcid',
         title: 'ORCID',
         section: 'Socials',
