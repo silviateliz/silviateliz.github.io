ead584aa4c7f1261fc4df3127de62dec74de4212
silviateliz <silvia.teliz@asc.upenn.edu>
2026-08-27 15:36:06 +0000

    Deploying to gh-pages from @ silviateliz/silviateliz.github.io@b0ce6bf662c84b0a4dbbd3d4fb84f857de9495d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,6 +106,13 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-8958-6372", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=bEIbJF8AAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
